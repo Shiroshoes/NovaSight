fe3e3067019c25945efa3ddfbefd16acd226a87f
Jeo-Criz Izzack E. Perdio <jeocriz@gmail.com>
2026-06-26 17:10:55 +0800

    New update

--- configs/config.py
+++ configs/config.py
@@ -1,21 +1,23 @@
 import os
 
-#roooottt
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-# Flask & DB
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'nova.db')
+
+DB_DIR = os.path.join(BASE_DIR, 'database')
+
+if not os.path.exists(DB_DIR):
+    os.makedirs(DB_DIR)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(DB_DIR, 'nova.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'your_secret_key_here'
 
-# File upload — app/static/uploads/
 UPLOAD_FOLDER = os.path.join(BASE_DIR, 'app', 'static', 'uploads')
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-MAX_CONTENT_LENGTH = 5 * 1024 * 1024  # 5MB max
+MAX_CONTENT_LENGTH = 5 * 1024 * 1024 # 5MB max
 
-# User roles
 ALLOWED_ROLES = [
-    'admin', 'Registrar', 'SASO', 'Student_Affair',
-    'CAHSdean', 'CBAdean', 'CCSTdean',
-    'CEAdean', 'CoASdean', 'CTECdean'
+    'admin', 'Registrar', 'SASO', 'Student_Affair', 
+    'CAHSdean', 'CBAdean', 'CCSTdean', 'CEAdean', 
+    'CoASdean', 'CTECdean'
 ]
